d8d62171a4720aa840d340d4196ad97c7ff49a44
Illya Yalovyy <yalovoy@gmail.com>
2024-02-10 15:21:11 -0800

    Partial fix for bioreservoir gui

--- src/main/java/org/jd/infestusfrontier/screen/BioReservoirScreen.java
+++ src/main/java/org/jd/infestusfrontier/screen/BioReservoirScreen.java
@@ -2,6 +2,7 @@ package org.jd.infestusfrontier.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -11,10 +12,12 @@ import org.jd.infestusfrontier.InfestusFrontier;
 
 public class BioReservoirScreen extends AbstractContainerScreen<BioReservoirMenu> {
     public static final ResourceLocation TEXTURE =
-            new ResourceLocation(InfestusFrontier.MODID, "textures/gui/corruption_core_biomass_gui.png");
+            new ResourceLocation(InfestusFrontier.MODID, "textures/gui/biomass_container_gui.png");
 
     public BioReservoirScreen(BioReservoirMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component);
+        this.imageWidth = 184;
+        this.imageHeight = 184;
     }
 
     @Override
@@ -27,11 +30,9 @@ public class BioReservoirScreen extends AbstractContainerScreen<BioReservoirMenu
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
-
-        this.blit(stack, x, y, 0, 0, imageWidth, imageHeight);
-
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
+        blit(stack, x, y, 0, 0, this.imageWidth, this.imageHeight);
     }
 
     @Override
