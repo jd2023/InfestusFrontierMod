cf24722d91c960efe12210fefab39ca15d278ee7
jd2023 <jd.yalovyy@gmail.com>
2024-03-12 17:43:33 -0700

    -did something

--- src/main/java/org/jd/infestusfrontier/screen/CorruptionCoreScreen.java
+++ src/main/java/org/jd/infestusfrontier/screen/CorruptionCoreScreen.java
@@ -8,7 +8,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.jd.infestusfrontier.InfestusFrontier;
-import org.jd.infestusfrontier.utils.MouseUtil;
+
 
 import java.util.Optional;
 
