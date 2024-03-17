6a03e900f8da4c700c13b8c6129ee2337e4cf3e5
jd2023 <jd.yalovyy@gmail.com>
2024-03-17 09:59:06 -0700

    -Added and edited all json files for Loot Leech

--- src/main/java/org/jd/infestusfrontier/block/InfesusLootLeech.java
+++ src/main/java/org/jd/infestusfrontier/block/InfesusLootLeech.java
@@ -2,13 +2,14 @@ package org.jd.infestusfrontier.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import org.jetbrains.annotations.Nullable;
 
 public class InfesusLootLeech extends BaseEntityBlock {
-    public static final String ID = "infestuslootleech";
+    public static final String ID = "infestus_loot_leech";
 
     public InfesusLootLeech() {
         super(Properties.of(Material.STONE));
@@ -18,5 +19,9 @@ public class InfesusLootLeech extends BaseEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
         return null;
+    }
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 }
