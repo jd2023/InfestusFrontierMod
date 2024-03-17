84ee1327bca154d5b52077c352cba6b60ade385e
jd2023 <jd.yalovyy@gmail.com>
2024-03-16 18:58:13 -0700

    -Made Helper Interface for Loot Leech

---
+++ src/main/java/org/jd/infestusfrontier/block/entity/util/InfestusLootLeech.java
@@ -0,0 +1,19 @@
+package org.jd.infestusfrontier.block.entity.util;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public interface InfestusLootLeech {
+    VoxelShape SUCK = Block.box(-48.0D, 96.0D, -48.0D, 104.0D, 96.0D, 104.0D);
+
+    default VoxelShape getSuckShape() {
+        return SUCK;
+    }
+
+    double getLevelX();
+
+    double getLevelY();
+
+    double getLevelZ();
+}
