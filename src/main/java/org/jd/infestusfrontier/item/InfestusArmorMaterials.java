7d1d13ab9122559d72aa8c3f8ab6a5c3580b500e
jd2023 <jd.yalovyy@gmail.com>
2024-10-22 17:50:39 -0700

    -started adding armor

---
+++ src/main/java/org/jd/infestusfrontier/item/InfestusArmorMaterials.java
@@ -0,0 +1,75 @@
+package org.jd.infestusfrontier.item;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jd.infestusfrontier.InfestusFrontier;
+
+
+import java.util.function.Supplier;
+
+public enum InfestusArmorMaterials implements ArmorMaterial {
+    FLESH_TIER_ONE("flesh_tier_one", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ZOMBIE_INFECT, 0.0F, 0.0F,
+            () -> Ingredient.of(InfestusItems.FERMENTED_FLESH.get()));
+
+
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    InfestusArmorMaterials(String p_40474_, int p_40475_, int[] p_40476_, int p_40477_, SoundEvent p_40478_, float p_40479_, float p_40480_, Supplier<Ingredient> p_40481_) {
+        this.name = p_40474_;
+        this.durabilityMultiplier = p_40475_;
+        this.slotProtections = p_40476_;
+        this.enchantmentValue = p_40477_;
+        this.sound = p_40478_;
+        this.toughness = p_40479_;
+        this.knockbackResistance = p_40480_;
+        this.repairIngredient = new LazyLoadedValue<>(p_40481_);
+    }
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type type) {
+        return HEALTH_PER_SLOT[type.getSlot().getIndex()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type type) {
+        return this.slotProtections[type.getSlot().getIndex()];
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    public String getName() {
+        return InfestusFrontier.MODID + ":" + this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+
+}
